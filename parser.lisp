014df1a37d1963d716ec913323035404606ee3bc
Philip Crockett <philipmayoo@gmail.com>
2018-04-26 16:58:45 +0200

    Psudeo code skeleton

--- parser.lisp
+++ parser.lisp
@@ -1,1 +1,16 @@
-(defun asd)
+(defun peek (x)
+  ;; Peek at the next character in the stream without consuming it
+  )
+
+(defun expect (x)
+  ;; Expect a specified character in the input, error if it does not exist
+  )
+
+(defun read()
+  ;; Read and returns a character from the input
+  )
+
+
+
+
+  
