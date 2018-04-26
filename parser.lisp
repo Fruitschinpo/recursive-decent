2100b85d9143a36ada1f524d2127877919c91013
Philip Crockett <philipmayoo@gmail.com>
2018-04-26 17:20:32 +0200

    Setup reading from a file and outputing its content

--- parser.lisp
+++ parser.lisp
@@ -6,9 +6,16 @@
   ;; Expect a specified character in the input, error if it does not exist
   )
 
-(defun read()
+(defun lexer()
   ;; Read and returns a character from the input
   )
+
+(defun parse ()
+  ;; Parse a file and according to a specified grammar
+  (with-open-file (stream "test.txt" :if-does-not-exist nil)
+    (loop for line = (read-line stream nil :eof)
+       until (eq line :eof)
+	 do (format t "~$~%" line))))
 
 
 
